bd8df92a709506989ef482529d4cba101d1f9bb9
vikasnetbeamerp <info@netbeam.in>
2016-12-29 00:34:59 +0530

    uploaded v

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+//Database details 
+define ('DB_HOST', 'localhost'); 
+//Username 
+define ('DB_USERNAME', 'root'); 
+//Pass 
+define ('DB_PASS', 'root'); 
+//Database Name 
+define ('DB_NAME', 'hoosk'); 
+//Base URL 
+define ('BASE_URL', 'http://localhost/Hoosk-master'); 
+//Email/Cookie URL 
+define ('EMAIL_URL', 'localhost/Hoosk-master'); 
+?>
